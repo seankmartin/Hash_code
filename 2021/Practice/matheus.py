c25e3aea41bb827a2001b535e3cfda06f9c8c3c0
cafalchio <maolivei@tcd.ie>
2021-01-25 22:56:52 +0000

    updated Matheus solution

--- 2021/Practice/matheus.py
+++ 2021/Practice/matheus.py
@@ -1,7 +1,22 @@
 # TODO import your packages
 import numpy as np
-
+import operator
 from common import scorer
+from sklearn.preprocessing import OrdinalEncoder as oe
+
+def ord_pizzas(pizzas):
+    oe = OrdinalEncoder()
+    ae.fit(pizzas)
+    enc_pizzas = oe.transform(pizzas)
+    pizza_idxs = np.array([i for i in range(len(pizzas))])
+    return ord_pizzas, old_id, new_id
+
+class Pizza():
+    def __init__(self, pizza, idx):
+        self.score = len(set(pizza))
+        self.idx = idx
+    def __repr__(cls):
+        return 'pizza :)'
 
 
 def matheus_solution(info, **kwargs):
@@ -10,8 +25,15 @@ def matheus_solution(info, **kwargs):
 
     # TODO decide number of choices and do without replacement
     pizza_idxs = np.array([i for i in range(len(pizzas))])
+
+    new_pizzas = []
+    for i, val in enumerate(pizzas):
+        new_pizzas.append(Pizza(val, i))
+
+    sorted_new_pizzas = sorted(new_pizzas, key=operator.attrgetter('score'), reverse=True)
     
-    np.random.shuffle(pizza_idxs)
+    # np.random.shuffle(pizza_idxs)
+
     draw_arr = []
     for i in range(T2):
         draw_arr.append(2)
@@ -24,8 +46,8 @@ def matheus_solution(info, **kwargs):
     curr_idx = 0
     for val in draw_arr:
         end_idx = curr_idx + val
-        if end_idx <= len(pizzas):
-            choices = list(pizza_idxs[curr_idx:end_idx])
+        if end_idx <= len(sorted_new_pizzas):
+            choices = [ n.idx for n in sorted_new_pizzas[curr_idx:end_idx]]
             team_size = val
             new_entry = [team_size,] + choices
             solution.append(new_entry)
